d405866969ac733c0615a75ef8eb1f44da783473
mustafa@mobilefringe.com @ CodeCloud <mustafa@mobilefringe.com @ CodeCloud>
2019-09-06 12:45:59 -0400

    Vista Village 1.1  /header.vue

--- header.vue
+++ header.vue
@@ -69,6 +69,15 @@
                                             </div>
         							    </li>
         					        </ul>
+        					        <div class="mobile_social_icon_container">
+                                       <p class="social_title">Follow Us on Social Media</p>
+                                       <span class="social_icon" v-for="item in social_media">
+                                           <a :href="item.url" target="_blank">
+                                               <p class="accessibility">{{item.name}}</p>
+                                               <i :class="item.iconClass" aria-hidden="true"></i>
+                                           </a>
+                                       </span>
+                                   </div>
         						</nav>
         				    </transition>
         				</div>
